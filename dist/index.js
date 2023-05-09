967fe235616b2e70230ffda76739e931de33045f
Harsh Srivastav <hsrivastav45@gmail.com>
2023-05-10 02:21:14 +0530

    Classes

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,10 @@
 var User = /** @class */ (function () {
     function User(email, name) {
+        this.city = "Varanasi";
         this.email = email;
         this.name = name;
     }
     return User;
 }());
+var harsh = new User("h@s.com", "Harsh");
+console.log("".concat(harsh.name, "   ").concat(harsh.city, "  ").concat(harsh.email));
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 class User {
     email: string
     name: string
-    
+    readonly city: string = "Varanasi"
     constructor(email: string, name: string) {
         this.email = email
         this.name = name
     }
 }
+
+const harsh = new User ("h@s.com", "Harsh")
+console.log(`${harsh.name}   ${harsh.city}  ${harsh.email}`)
