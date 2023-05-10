be256a6ebcc34f7fa24e38d300a7a0fdabca8709
Harsh Srivastav <hsrivastav45@gmail.com>
2023-05-10 18:41:09 +0530

    Getters and Setters

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,37 @@
 class User {
-    email;
+    email; //Public is optional
     name;
-    city = "Varanasi"; //Not taking in constructor,
+    token = "";
+    city = "Varanasi"; //Not taking in constructor,hence not important to specify while creating object
     constructor(email, name) {
         this.email = email;
         this.name = name;
+    }
+}
+//How modern devs write TS classes, compact way
+class Usr {
+    email;
+    name;
+    token;
+    _courseCount = 1;
+    city = "Varanasi";
+    constructor(email, name, token) {
+        this.email = email;
+        this.name = name;
+        this.token = token;
+    }
+    //Getter
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get getCourseCount() {
+        return this._courseCount;
+    }
+    set setCourseCount(courseNum) {
+        this._courseCount = courseNum;
+    }
+    deleteToken() {
+        console.log("Token Deleted");
     }
 }
 const harsh = new User("h@s.com", "Harsh");
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,44 @@
 class User {
-    email: string
-    name: string
+    public email: string //Public is optional
+    public name: string
+    private token: string = ""
     readonly city: string = "Varanasi" //Not taking in constructor,hence not important to specify while creating object
     constructor(email: string, name: string) {
         this.email = email
         this.name = name
     }
+}
+
+//How modern devs write TS classes, compact way
+
+class Usr {
+
+    private _courseCount = 1
+    readonly city: string = "Varanasi"
+    constructor(
+        public email: string,
+        public name: string,
+        private token: string
+    ) {
+    }
+
+    //Getter
+    get getAppleEmail(): string {
+        return `apple${this.email}`
+    }
+
+    get getCourseCount(): number {
+        return this._courseCount
+    }
+
+    set setCourseCount(courseNum: number){
+        this._courseCount = courseNum
+    }
+
+    private deleteToken() {
+        console.log("Token Deleted")
+    }
+
 }
 
 const harsh = new User ("h@s.com", "Harsh")
