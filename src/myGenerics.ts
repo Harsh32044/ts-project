c5c469090caa17b215c87a7537915c89d609e1f7
Harsh Srivastav <hsrivastav45@gmail.com>
2023-05-12 02:55:53 +0530

    More on Generics

--- src/myGenerics.ts
+++ src/myGenerics.ts
@@ -5,8 +5,34 @@ function identity(arg: any): any {
 
 //Better way
 function identity2<Type> (arg: Type): Type {
+    //Type info is not lost, same type is being taken and returned
     return arg
 }
 
 const score: Array<number> = []
 const names: Array<string> = []
+
+//Modern writing
+function ID4<T>(val: T): T {
+    return val
+}
+
+//Advantage is, we can pass on our own types, defined via Interfaces
+
+interface Bottle {
+    brand: string,
+    type: number
+}
+
+const newB: Bottle = {
+    brand: "Coke",
+    type: 3
+}
+//Type can be anything
+function identity4<Type>(): Type {
+    
+    const myBottle: Type = { brand: 'MyBrand', type: 1 } as Type
+    return myBottle
+}
+//In JS version its a simple object getting returned
+console.log(identity4())
